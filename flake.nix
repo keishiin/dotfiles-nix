364bcf37e0e648d27e818ca4144f3b1d1c0ecd5b
keishiin <inferno2960@gmail.com>
2024-04-19 23:31:04 -0400

    this took so long and still doesnt work like i want it to

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "My first flake!";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        KEISHIN = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+      homeConfigurations = {
+        keishin = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+        };
+      };
+    };
+}
+
+
