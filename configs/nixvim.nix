78c4e9e06e5c558da1c0535311600c7e942ae672
keishiin <inferno2960@gmail.com>
2024-04-23 22:16:59 -0400

    nixvim set up

--- configs/nixvim.nix
+++ configs/nixvim.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, inputs, nixvim, ... }:
-
 {
-
+  config,
+  pkgs,
+  inputs,
+  nixvim,
+  ...
+}: {
   # using nixvim instead of what ever i was doing before
   programs.nixvim = {
     enable = true;
@@ -9,15 +12,158 @@
 
     colorschemes.catppuccin = {
       enable = true;
-      settings.transparent_background = true;
+      settings = {
+        transparent_background = true;
+        flavour = "mocha";
+        no_underline = true;
+        integrations = {
+          cmp = true;
+          nvimtree = true;
+          treesitter = true;
+        };
+      };
     };
 
     plugins = {
       lualine.enable = true;
-      telescope.enable = true;
       oil.enable = true;
-      treesitter.enable = true;
-      luasnip.enable = true;
+      trouble.enable = true;
+
+      lspsaga = {
+        enable = true;
+        beacon = {
+          enable = true;
+        };
+        ui = {
+          border = "rounded";
+          codeAction = "💡";
+        };
+        hover = {
+          openCmd = "!kitty -e";
+          openLink = "firefox";
+        };
+        diagnostic = {
+          borderFollow = true;
+          diagnosticOnlyCurrent = false;
+          showCodeAction = true;
+        };
+        symbolInWinbar.enable = true; # Breadcrumbs
+        codeAction = {
+          extendGitSigns = false;
+          showServerName = true;
+          onlyInCursor = true;
+          numShortcut = true;
+          keys = {
+            exec = "<CR>";
+            quit = ["<Esc>" "q"];
+          };
+        };
+        lightbulb = {
+          enable = false;
+          sign = false;
+          virtualText = true;
+        };
+        implement.enable = false;
+        rename = {
+          autoSave = false;
+          keys = {
+            exec = "<CR>";
+            quit = ["<C-k>" "<Esc>"];
+            select = "x";
+          };
+        };
+        outline = {
+          autoClose = true;
+          autoPreview = true;
+          closeAfterJump = true;
+          layout = "normal"; # normal or float
+          winPosition = "right"; # left or right
+          keys = {
+            jump = "e";
+            quit = "q";
+            toggleOrJump = "o";
+          };
+        };
+        scrollPreview = {
+          scrollDown = "<C-f>";
+          scrollUp = "<C-b>";
+        };
+      };
+
+      none-ls = {
+        enable = true;
+        enableLspFormat = true;
+        updateInInsert = false;
+        sources = {
+          code_actions = {
+            gitsigns.enable = true;
+            statix.enable = true;
+          };
+          diagnostics = {
+            statix.enable = true;
+            yamllint.enable = true;
+          };
+          formatting = {
+            alejandra.enable = true;
+            black = {
+              enable = true;
+              withArgs = ''
+                {
+                  extra_args = { "--fast" },
+                }
+              '';
+            };
+            prettier = {
+              enable = true;
+              disableTsServerFormatter = true;
+              withArgs = ''
+                {
+                  extra_args = { "--no-semi", "--single-quote" },
+                }
+              '';
+            };
+            stylua.enable = true;
+            yamlfmt.enable = true;
+          };
+        };
+      };
+
+      neo-tree = {
+        enable = true;
+        enableGitStatus = true;
+        enableModifiedMarkers = true;
+        example.window.mappings = {
+          "<leader>" = "toggle_node";
+          "e" = ":Neotree toggle float";
+          d = "open neo tree";
+        };
+      };
+
+      treesitter = {
+        enable = true;
+        indent = true;
+        folding = true;
+        nixvimInjections = true;
+        grammarPackages = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+          nix
+          rust
+          python
+          markdown
+        ];
+      };
+
+      telescope = {
+        enable = true;
+        highlightTheme = "catppuccin-mocha";
+        extensions = {
+          file-browser.enable = true;
+          fzf-native.enable = true;
+        };
+        settings.defaults = {
+          layout_config.horizontal.prompt_position = "top";
+          sorting_strategy = "ascending";
+        };
+      };
 
       conform-nvim = {
         enable = true;
@@ -27,24 +173,50 @@
         };
         notifyOnError = true;
         formattersByFt = {
-          python = [ "black" ];
-          nix = [ "alejandra" ];
-          markdown = [ [ "prettierd" "prettier" ] ];
-          rust = [ "rustfmt" ];
+          python = ["black"];
+          nix = ["alejandra"];
+          markdown = [["prettierd" "prettier"]];
+          rust = ["rustfmt"];
         };
       };
-      
-      lsp.servers = {
-        nixd = {
-          enable = true;
-          settings.formatting.command = "nixpkgs-fmt";
-        };
 
-        rust-analyzer = {
-          enable = true;
-          installCargo = false;
-          installRustc = false;
+      lsp-format = {enable = true;};
+      lsp = {
+        enable = true;
+        servers = {
+          nil_ls.enable = true;
+          marksman.enable = true;
+          ltex.enable = true;
+          pyright.enable = true;
+          gleam.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installCargo = false;
+            installRustc = false;
+          };
         };
+      };
+
+      lspkind = {
+        enable = true;
+        extraOptions = {
+          maxwidth = 50;
+          ellipsis_char = "...";
+        };
+      };
+
+      luasnip = {
+        enable = true;
+        extraConfig = {
+          enable_autosnippets = true;
+          store_selection_keys = "<Tab>";
+        };
+        fromVscode = [
+          {
+            lazyLoad = true;
+            paths = "${pkgs.vimPlugins.friendly-snippets}";
+          }
+        ];
       };
 
       cmp = {
@@ -53,10 +225,10 @@
           autoEnableSources = true;
           experimental.ghost_text = true;
           sources = [
-            { name = "nvim_lsp"; }
-            { name = "path"; }
-            { name = "buffer"; }
-            { name = "luasnip"; }
+            {name = "nvim_lsp";}
+            {name = "path";}
+            {name = "buffer";}
+            {name = "luasnip";}
           ];
 
           mapping = {
@@ -72,10 +244,10 @@
       };
 
       cmp-buffer.enable = true;
-      cmp-cmdline.enable = true; 
+      cmp-cmdline.enable = true;
       cmp-emoji.enable = true;
-      cmp_luasnip.enable = true; 
-      cmp-nvim-lsp.enable = true; 
+      cmp_luasnip.enable = true;
+      cmp-nvim-lsp.enable = true;
       cmp-path.enable = true;
     };
 
